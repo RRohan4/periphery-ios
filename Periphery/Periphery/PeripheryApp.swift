dd11812f07039ce9fd94a886c472cd866fb5ebe4
Rohan Raina <rohan.raina04@gmail.com>
2026-09-01 11:05:50 -0400

    Xcode project with models, sources and goldens

---
+++ Periphery/Periphery/PeripheryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeripheryApp.swift
+//  Periphery
+//
+//  Created by Renuka.Raina on 2026-08-31.
+//
+
+import SwiftUI
+
+@main
+struct PeripheryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
